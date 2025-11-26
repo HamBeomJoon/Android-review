0d005f8a964412b008f75756bb124e0677a304f6
Ham BeomJoon <hbj0802@naver.com>
2025-11-26 18:51:35 +0900

    build: dependency graph generator plugin 추가
    - root `build.gradle.kts`에 `com.vanniktech.dependency.graph.generator` 플러그인 (v0.8.0) 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,5 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+    id("com.vanniktech.dependency.graph.generator") version "0.8.0"
 }
